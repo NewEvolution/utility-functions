93507368b0e1eb73069f40ed6ba36a8661e7810f
Ryan Tanay <ryantanay@newevolution.org>
2016-01-19 13:24:36 -0600

    Added description and timeout to tests

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,19 @@
 var expect = require("chai").expect;
 
-it("da_truth", function() {
-  expect(true).to.be.true;
+describe("Mocha & Chai", function() {
+  it("da_truth", function() {
+    expect(true).to.be.true;
+  });
 });
 
-it("arrays have length", function() {
-  var array = [1,2,3];
-  expect(array.length).to.exist;
+describe("async tests", function() {
+  it("arrays have length", function() {
+    var array = [1,2,3];
+    setTimeout(function() {
+      expect(array.length).to.exist;
+      done();
+    }, 0);
+  });
 });
 
 describe("Hello world", function() {
