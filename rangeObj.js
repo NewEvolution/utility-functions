736d91204f58b4d5f2c02f2e5822151e42c156a3
Ryan Tanay <ryantanay@newevolution.org>
2016-01-19 16:01:06 -0600

    Removed initial extraneous code, returned object directly instead of setting and returning a variable

--- rangeObj.js
+++ rangeObj.js
@@ -1,13 +1,8 @@
 module.exports = function rangeObj(start, end) {
   var rangeMaker = require("./range");
   var rangeArray = rangeMaker(start, end);
-  //var builtObj = {};
-  //for(item in rangeArray) {
-  //  builtObj[rangeArray[item]] = parseInt(rangeArray[item]);
-  //}
-  var builtObj = rangeArray.reduce(function(init, curr) {
+  return rangeArray.reduce(function(init, curr) {
     init[curr] = curr;
     return init;
   }, {});
-  return builtObj;
 };
