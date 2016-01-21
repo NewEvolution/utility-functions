22a8e6062a505e8c425fe1cb136fb9a988de1a5c
Ryan Tanay <ryantanay@newevolution.org>
2016-01-21 10:19:22 -0600

    Renamed some arguments to more clearly convey their use

--- rangeObj.js
+++ rangeObj.js
@@ -1,8 +1,8 @@
 module.exports = function rangeObj(start, end) {
   var rangeMaker = require("./range");
   var rangeArray = rangeMaker(start, end);
-  return rangeArray.reduce(function(init, curr) {
-    init[curr] = curr;
-    return init;
+  return rangeArray.reduce(function(providedObj, currentVal) {
+    providedObj[currentVal] = currentVal;
+    return providedObj;
   }, {});
 };
